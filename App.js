0119c2126afbcabff067f38ef96e24d25d4073a7
Janet Zheng <jzhengcode@gmail.com>
2021-06-07 16:33:31 -0700

    styling exercise completed

--- App.js
+++ App.js
@@ -3,27 +3,54 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Here are some boxes of different colours</Text>
+      <View style={[styles.cyanBox]}>
+        <Text style={styles.text}>Cyan #2aa198</Text>
+      </View>
+      <View style={[styles.blueBox]}>
+        <Text style={styles.text}>Blue #268bd2</Text>
+      </View>
+      <View style={[styles.magentaBox]}>
+        <Text style={styles.text}>Magenta #d33682</Text>
+      </View>
+      <View style={[styles.orangeBox]}>
+        <Text style={styles.text}>Orange #cb4b16</Text>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 2,
-  },
   container: {
-    flex: 1,
-    margin: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 50,
   },
-  pink: {
-    backgroundColor: 'pink',
+  text: {
+    color: 'white',
+  },
+  cyanBox: {
+    backgroundColor: '#2aa198',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  blueBox: {
+    backgroundColor: '#268bd2',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  magentaBox: {
+    backgroundColor: '#d33682',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  orangeBox: {
+    backgroundColor: '#cb4b16',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
   },
 });
