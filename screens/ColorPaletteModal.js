b7727e98caf6ab3336c60cd035133cfc8904323e
Janet Zheng <jzhengcode@gmail.com>
2021-06-08 17:36:57 -0700

    added submit and validate alert

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
-import { Text, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import {
+  Text,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+const COLORS = [
+  { colorName: 'AliceBlue', hexCode: '#F0F8FF' },
+  { colorName: 'AntiqueWhite', hexCode: '#FAEBD7' },
+  { colorName: 'Aqua', hexCode: '#00FFFF' },
+];
 
 export default function ColorPaletteModal() {
-  const [paletteName, setColorPalette] = useState('');
-  const [colors, setColors] = useState([]);
+  const [palette, setPalette] = useState({});
+  const [paletteName, setPaletteName] = useState('');
+  const [selectedColors, setSelectedColors] = useState([]);
+
+  const submit = () => {
+    if (!paletteName) {
+      Alert.alert('Please enter a palette name');
+    }
+  };
+
+  const handleSubmit = useCallback(submit, [paletteName]);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Name of your color palette</Text>
       <TextInput
         style={styles.input}
-        onChangeText={setColorPalette}
+        onChangeText={setPaletteName}
         value={paletteName}
       />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -20,11 +45,23 @@ export default function ColorPaletteModal() {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    backgroundColor: 'white',
+    flex: 1,
   },
   input: {
     height: 40,
     marginBottom: 20,
     borderWidth: 1,
     borderRadius: 5,
+  },
+  button: {
+    backgroundColor: 'teal',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
